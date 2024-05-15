87350283706b672eb3a9381187e6d1d54c639c1d
Aqsaa Qaazi <159919683+GD-aish0o@users.noreply.github.com>
2024-05-15 21:02:03 +0500

    uploaded code of num-chase.
    In this commit author has pushed code of a cli based number guessing game. The project is crafted in typescript language, compiled javascript code, config files and json files. the code can be run using node js environment. 
    
    install the project on CMD: npm i num-chase  or try running the pkg without installing by: npx num-chase

---
+++ index.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+// shebang
+import inquirer from "inquirer";
+const computerGenerated = Math.floor(Math.random() * 10);
+// line 8 variable stores the number generated randomly by computer
+// const in line 10 stores the user guesssed number.
+const userInput = await inquirer.prompt([
+    {
+        // an object, storing key value;
+        // key  : value
+        name: "guessedNumber",
+        type: "number",
+        message: "Guess a Number digit '0-9'",
+    },
+]);
+// putting on conditions to tell the user wether user was right or wrong.
+if (userInput.guessedNumber === computerGenerated) {
+    console.log("You're the winner!");
+    // this prints only if user guessed the right number.
+}
+else {
+    console.log(`Correct answer is ${computerGenerated} Better luck next time`);
+    // this prints only when the user guessed wrong number.
+}
